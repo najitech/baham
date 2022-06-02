0a1ae830c536dde2863327d978296d9a28d133a0
alirezashafaee <zerocode0026@gmail.com>
2022-06-02 13:51:21 +0430

    bar chart added to op page

--- src/Pages/Overview/Overview.js
+++ src/Pages/Overview/Overview.js
@@ -7,34 +7,87 @@ function Overview() {
     {
           
       series: [{
-        name: 'Inflation',
         data: [48,252,79,90,171,330,125]
       }],
 
       options: {
         chart: {
           type: 'bar',
-          height: 350
+          height: 350,
+          width:500,    
+          toolbar: {
+            show: false,}
         },
+        colors:["#0BA77D","#16629E","#F54456","#F54456","#16629E","#16629E","#F54456"],
         plotOptions: {
           bar: {
             horizontal: false,
             columnWidth: '45%',
-            endingShape: 'rounded'
+            endingShape: 'rounded',
+            dataLabels: {
+              position: 'top', 
+            },
           },
         },
         dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          show: true,
-          width: 2,
-          colors: ['transparent']
-        },
-        xaxis: {
-          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+          enabled: true,
+          offsetY: -24,
+          style: {
+            fontSize: '12px',
+            fontWeight:400,
+            fontFamily:'IranSansX',
+            colors: ["#807E7C"]
+          }
         },
         
+        stroke: {
+          show: false,
+        },
+        grid: {
+          show: false,
+        },
+        yaxis: {
+          show: true,
+          labels: {
+              show: false,
+          },
+          axisBorder: {
+              show: true,
+              width:1,
+              color: '#000000',
+              offsetX: 0,
+              offsetY: -1
+          },
+          axisTicks: {
+              show: false,
+          },
+          
+      },
+        xaxis: {
+          labels: {
+            show: true,
+            style: {
+                fontSize: '10px',
+                color:'#807E7C',
+                fontFamily: 'IranSansX',
+                fontWeight: 400,
+            },
+          },
+          axisTicks: {
+            show: false,
+        },
+        axisBorder: {
+          show: true,
+          color: '#000000',
+          height: 1,
+          width: '100%',
+          offsetX: 1,
+          offsetY: 0
+        },
+          categories: ['<50', '51-75', '76-100', '101-150', '151-200', '201-300', '301-500'],
+        },
+        
+     
         fill: {
           opacity: 1
         },
@@ -219,7 +272,6 @@ function Overview() {
           </div>
         </div>
       </div>
-
       <div className='tarakomContainer'>
           <div className='meduimheaderCardOp rightmeduimLableHeaderOp'>
             <div><h4><span>تراکم جمعیت کوچه </span> ارکیده </h4></div>
@@ -253,6 +305,11 @@ function Overview() {
           <div className='chartContainerBarShahrOp'>
             <Chart options={barCharshahr.options} series={barCharshahr.series} type="bar" height={170}/>
           </div>
+          <div className='labelShahrOp'><span>تعداد ملک با متراژ مختلف تا شعاع 100 متری کوچه ارکیده</span></div>
+          <div style={{marginTop:'10px'}} className='chartContainerBarShahrOp'>
+            <Chart options={barCharshahr.options} series={barCharshahr.series} type="bar" height={170}/>
+          </div>
+          <div className='labelShahrOp'><span>تعداد ملک با متراژ مختلف مرزداران</span></div>
       </div>
       <div className='baftkocheOp'>
           <div className='meduimheaderCardOp rightmeduimLableHeaderOp'>
