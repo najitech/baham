dd8fd25857c8675c096a69cddd5b105e172419fb
morteza80 <morteza.3921@gmail.com>
2022-06-09 18:20:17 +0430

    done

--- src/Pages/Overview/OverviewHeader/OverviewHeader.js
+++ src/Pages/Overview/OverviewHeader/OverviewHeader.js
@@ -7,41 +7,43 @@ import Barcode from '../../../overviewBarcode.png'
 function OverviewHeader() {
   return (
     <div className='OverviewHeader'>
-        <div className='OverviewHeaderRight'>
-            <img alt="logo" src={Logo} className="OverviewHeaderLogo"/>
-            <div className='OverviewHeaderLogoTextContainer'>
-                <img alt="logo" src={LogoText} className="OverviewHeaderLogoText"/>
-            </div>
-          
-   
-        </div>
-        <div className='OverviewHeaderCenter'>
-            <div className='OverviewHeaderCenterTop'>
-                <div className='OverviewHeaderCenterTopAbs'>
-                    <div className='OverviewHeaderCenterTopAbs_BahamContainer'>
-                        <h2 className='OverviewHeaderCenterTopAbs_Baham'>باهم</h2>
-                    </div>
+        <div className='OverviewHeaderBeforeRes'>
+            <div className='OverviewHeaderRight'>
+                <img alt="logo" src={Logo} className="OverviewHeaderLogo"/>
+                <div className='OverviewHeaderLogoTextContainer'>
+                    <img alt="logo" src={LogoText} className="OverviewHeaderLogoText"/>
+                </div>
             
-                    <div className='OverviewHeaderCenterTopAbs_Texts'>
-                        <h3><span>ب</span>رنامه</h3>
-                        <h3><span>ا</span>نتخاب</h3>
-                        <h3><span>ه</span>وشمندانه</h3>
-                        <h3><span>م</span>سکن</h3>
+    
+            </div>
+            <div className='OverviewHeaderCenter'>
+                <div className='OverviewHeaderCenterTop'>
+                    <div className='OverviewHeaderCenterTopAbs'>
+                        <div className='OverviewHeaderCenterTopAbs_BahamContainer'>
+                            <h2 className='OverviewHeaderCenterTopAbs_Baham'>باهم</h2>
+                        </div>
+                
+                        <div className='OverviewHeaderCenterTopAbs_Texts'>
+                            <h3><span>ب</span>رنامه</h3>
+                            <h3><span>ا</span>نتخاب</h3>
+                            <h3><span>ه</span>وشمندانه</h3>
+                            <h3><span>م</span>سکن</h3>
+                        </div>
                     </div>
                 </div>
+                <div className='OverviewHeaderCenterBottom'>
+                    <span>آدرس: تهران بلوار مرزداران خیابان سرسبز جنوبی پلاک 1</span>
+                    <span>تلفن: 44262200</span>
+                </div>
             </div>
-            <div className='OverviewHeaderCenterBottom'>
-                <span>آدرس: تهران بلوار مرزداران خیابان سرسبز جنوبی پلاک 1</span>
-                <span>تلفن: 44262200</span>
+            <div className='OverviewHeaderLeft'>
+                <img alt="logo" src={Barcode} className="OverviewHeaderBarcode"/>
+                <div className='OverviewHeaderLeftTitleContainer'>
+                    <span className='OverviewHeaderLeftTitle'>اسکن کنید</span>
+                </div>
+                
+                
             </div>
-        </div>
-        <div className='OverviewHeaderLeft'>
-            <img alt="logo" src={Barcode} className="OverviewHeaderBarcode"/>
-            <div className='OverviewHeaderLeftTitleContainer'>
-                <span className='OverviewHeaderLeftTitle'>اسکن کنید</span>
-            </div>
-            
-            
         </div>
     </div>
   )
